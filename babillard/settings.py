106a9629bdc404afdd0701b938e54cf6e61db71b
abdelaziz saroui <saroui2015gmail.com>
2020-09-21 14:09:13 +0100

    add whitenoise configs

--- babillard/settings.py
+++ babillard/settings.py
@@ -144,8 +144,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# MEDIA_LOCATION = "media"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MEDIA_LOCATION = "media"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- babillard/urls.py
+++ babillard/urls.py
@@ -59,4 +59,5 @@ urlpatterns = [
     path('abonn/<int:page_id>/', pages_views.abonn, name='abonn'),
     path('add-memb/<int:page_id>/', pages_views.add_memb, name='add-memb')
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
